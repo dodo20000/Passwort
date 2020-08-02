82772566087c8aa2fcfdc736073c281d0196d2d5
dodo20000 <68115922+dodo20000@users.noreply.github.com>
2020-08-02 12:05:18 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function Passwort_Auswerten() {
     var x = document.getElementById("Eingang").value;
     var y = 1;
+    document.getElementById("Eingang").value = "";
     if ( x == y ) {
         document.getElementById("Passwort_antwort").innerHTML = "richtiges Passwort";
         document.getElementById("Passwort_antwort").style.color = "green";
