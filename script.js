bc96558baf76e582b64b10ef0d0722df3f4f442a
dodo20000 <68115922+dodo20000@users.noreply.github.com>
2020-08-02 11:53:11 +0200

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+function Passwort_Auswerten() {
+    var x = document.getElementById("Eingang").value;
+    var y = 1;
+    if ( x == y ) {
+        document.getElementById("Passwort_antwort").innerHTML = "richtiges Passwort";
+        document.getElementById("Passwort_antwort").style.color = "green";
+        document.getElementById("Ergebnis").innerHTML = "Ergebnis";
+    }
+    else {
+        document.getElementById("Passwort_antwort").innerHTML = "falsches Passwort";
+        document.getElementById("Passwort_antwort").style.color = "red";
+    }
+
+}
